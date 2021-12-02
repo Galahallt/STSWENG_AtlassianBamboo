b29608500b1978e528c5e73cbd7b70b67eb9b9f5
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2021-12-02 17:29:02 +0800

    added status attribute to instructor model

---
+++ server/src/model/Instructor.js
@@ -0,0 +1,60 @@
+// import mongoose module for schema modelling
+const mongoose = require('mongoose');
+
+const InstructorSchema = new mongoose.Schema(
+  {
+    instructorID: {
+      type: String,
+      required: true,
+    },
+
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    dlsuEmail: {
+      type: String,
+      required: true,
+    },
+
+    college: {
+      type: String,
+      required: true,
+    },
+
+    department: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      required: true,
+      default: "Active"
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    timestamp: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { timestamps: true }
+);
+
+// implement instructor schema as instructor model
+const InstructorModel = mongoose.model('Instructor', InstructorSchema);
+
+// export instructor model for data creation and manipulation
+export default InstructorModel;
