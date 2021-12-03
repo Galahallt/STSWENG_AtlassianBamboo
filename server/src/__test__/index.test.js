9cc92b1078360d670a702d3c4f5712c06336e873
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2021-12-03 19:31:05 +0800

    initialize for unit testing

--- server/src/__test__/index.test.js
+++ server/src/__test__/index.test.js
@@ -9,43 +9,25 @@ import User from '../model/User.js';
 
 // afterAll(async () => await mockDB.closeDatabase());
 
-describe('User Creation', () => {
-  it('First User', async () => {
-    try {
-      const user = {
-        fullName: 'Keil Finez',
-        givenName: 'Keil Christopher',
-        familyName: 'Finez',
-        email: 'keilFinez@gmail.com',
-        accessToken: 'asdasdasdasd',
-      };
-      expect(user.fullName).toEqual('Keil Finez');
-      done();
-    } catch (error) {
-      console.log(error);
-    }
+test('respond with status code 400', async (done) => {
+  const user = {
+    fullName: 'Keil Finez',
+    givenName: 'Keil Christopher',
+    familyName: 'Finez',
+    email: 'keilFinez@gmail.com',
+    accessToken: 'asdasdasdasd',
+  };
+  const response = await request(app).post('/login').send({
+    fullName: 'Keil Finez',
+    givenName: 'Keil Christopher',
+    familyName: 'Finez',
+    email: 'keilFinez@gmail.com',
+    accessToken: 'asdasdasdasd',
   });
-});
+  expect(response.statusCode).toBe(400);
 
-// test('respond with status code 400', async (done) => {
-//   const user = {
-//     fullName: 'Keil Finez',
-//     givenName: 'Keil Christopher',
-//     familyName: 'Finez',
-//     email: 'keilFinez@gmail.com',
-//     accessToken: 'asdasdasdasd',
-//   };
-//   const response = await request(app).post('/login').send({
-//     fullName: 'Keil Finez',
-//     givenName: 'Keil Christopher',
-//     familyName: 'Finez',
-//     email: 'keilFinez@gmail.com',
-//     accessToken: 'asdasdasdasd',
-//   });
-//   expect(response.statusCode).toBe(400);
-
-//   done();
-// });
+  done();
+}, 30000);
 
 // describe('POST /login', () => {
 //   describe('logging in with non-dlsu email', () => {
