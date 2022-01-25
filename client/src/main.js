d7449789780ac40f87362d96b613bbf2481a5cdf
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2022-01-25 18:20:32 +0800

    Revert "Delete client directory"
    This reverts commit d62b66f534e34e0f4821f0c84f61bded52dda969.

---
+++ client/src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import gAuth from 'vue3-google-oauth2';
+import './styles/index.css';
+import vue3StarRatings from "vue3-star-ratings";
+
+const app = createApp(App);
+
+let gAuthClientId =
+  '231543448601-uol3p2k1r1u5dc17l6uqnrkoqlsgoeiv.apps.googleusercontent.com';
+
+const gAuthOptions = {
+  clientId: gAuthClientId,
+  scope: 'email',
+  prompt: 'consent',
+  fetch_basic_profile: true,
+};
+
+app.component("vue3-star-ratings", vue3StarRatings);
+app.use(router);
+app.use(store);
+app.use(gAuth, gAuthOptions);
+app.mount('#app');
