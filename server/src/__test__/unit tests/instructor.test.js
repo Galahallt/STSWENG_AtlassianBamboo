533f5b31b9f8eec40cfd35672018ab12cf96713d
feilkinez <75008462+feilkinez@users.noreply.github.com>
2022-02-02 00:42:20 +0800

    semi-done with unit tests

--- server/src/__test__/unit tests/instructor.test.js
+++ server/src/__test__/unit tests/instructor.test.js
@@ -8,18 +8,21 @@ describe('Test Instructor Routes', () => {
     mongoose.disconnect();
   });
 
-  // it('invalid DLSU email, should respond with 400 status code', async () => {
-  //   const response = await request.app.post('/professor/addProf').send({
-  //     id: asiduasio91213,
-  //     firstName: 'Paolo',
-  //     lastName: 'Espiritu',
-  //     email: 'paoloEspiritu@gmail.com',
-  //     college: 'CCS',
-  //     department: 'Software Technology',
-  //     courses: ['STSWENG', 'CSSWENG'],
-  //   });
-  //   expect(response.statusCode).toBe(400);
-  // })
+  // to modify
+  // it('valid professor, should respond with 200 status code', async () => {
+  //   const response = await request(app)
+  //     .post('/professor/addProf')
+  //     .send({
+  //       id: 'asidasio91213',
+  //       lastName: 'Espiritu',
+  //       firstName: 'Paolo',
+  //       email: 'paoloEspiritu@dlsu.edu.ph',
+  //       college: 'CCS',
+  //       department: 'Software Technology',
+  //       courses: ['STSWENG', 'CSSWENG'],
+  //     });
+  //   expect(response.statusCode).toBe(200);
+  // });
 
   it('bad request, should respond with 500 status code', async () => {
     const response = await request(app)
@@ -28,47 +31,36 @@ describe('Test Instructor Routes', () => {
     expect(response.statusCode).toBe(500);
   });
 
-  it('no newProf was added, should respond with 400 status code', async () => {
-    const response = await request.app.post('/professor/addProfsCsv').send({
-      newProf: [],
-    });
-    expect(response.statusCode).toBe(400);
-  });
+  // it('no newProf was added, should respond with 400 status code', async () => {
+  //   const response = await request.app.post('/professor/addProfsCsv').send({
+  //     newProf: [],
+  //   });
+  //   expect(response.statusCode).toBe(400);
+  // });
 
   it('bad request, should respond with 500 status code', async () => {
     const response = await request(app)
       .post('/professor/addProfsCsv')
       .send({
-        error: new Error('Server Error'),
+        file: new Error('Server Error'),
       });
     expect(response.statusCode).toBe(500);
   });
 
-  it('no profs returned, should respond with 400 status code', async () => {
-    const response = await request.app.get('/professor/getAllProfs').send({
-      profs: [],
-    });
-    expect(response.statusCode).toBe(400);
+  it('function called, should respond with 200 status code', async () => {
+    const response = await request(app).get('/professor/getAllProfs');
+    expect(response.statusCode).toBe(200);
   });
 
-  it('bad request, should respond with 500 status code', async () => {
-    const response = await request(app)
-      .post('/professor/addProfsCsv')
-      .send({ error: new Error('Server Error') });
-    expect(response.statusCode).toBe(500);
-  });
+  // to modify
+  // it('function called, should respond with 200 status code', async () => {
+  //   const response = await request(app).get('/professor/12omj8v4kz4b0ot4');
+  //   expect(response.statusCode).toBe(200);
+  // });
 
-  it('no prof returned, should respond with 400 status code', async () => {
-    const response = await request.app.get('/professor/:profID').send({
-      prof: '',
-    });
-    expect(response.statusCode).toBe(400);
-  });
-
-  it('bad request, should respond with 500 status code', async () => {
-    const response = await request(app)
-      .post('/professor/:profID')
-      .send({ error: new Error('Server Error') });
-    expect(response.statusCode).toBe(500);
-  });
+  // to modify
+  // it('professor does not exist, should respond with 400 status code', async () => {
+  //   const response = await request(app).get('/professor/asdasdjklhasd');
+  //   expect(response.statusCode).toBe(400);
+  // });
 });
--- server/src/__test__/unit tests/rate.test.js
+++ server/src/__test__/unit tests/rate.test.js
@@ -8,12 +8,31 @@ describe('Test Rating Routes', () => {
     mongoose.disconnect();
   });
 
+  // to modify
+  // it('rating found, should respond with 200 status code', async () => {
+  //   const response = await request(app).post('/rating/findRating').send({
+  //     userEmail: 'keilFinez@dlsu.edu.ph',
+  //     profEmail: 'paoloEspiritu@dlsu.edu.ph',
+  //   });
+  //   expect(response.statusCode).toBe(200);
+  // });
+
   it('bad request, should respond with 500 status code', async () => {
     const response = await request(app)
       .post('/rating/findRating')
       .send({ error: new Error('Server Error') });
     expect(response.statusCode).toBe(500);
   });
+
+  // to modify
+  // it('add prof successful, should respond with 200 status code', async () => {
+  //   const response = await request(app).post('/rating/addRating').send({
+  //     rating: 4,
+  //     userEmail: 'keilFinez@dlsu.edu.ph',
+  //     instructorEmail: 'paoloEspiritu@dlsu.edu.ph',
+  //   });
+  //   expect(response.statusCode).toBe(200);
+  // });
 
   it('bad request, should respond with 500 status code', async () => {
     const response = await request(app)
@@ -22,16 +41,36 @@ describe('Test Rating Routes', () => {
     expect(response.statusCode).toBe(500);
   });
 
-  it('bad request, should respond with 500 status code', async () => {
-    const response = await request(app)
-      .post('/rating/getInstructorRatings')
-      .send({ error: new Error('Server Error') });
-    expect(response.statusCode).toBe(500);
-  });
+  // to modify
+  // it('professor found, should respond with 200 status code', async () => {
+  //   const response = await request(app)
+  //     .patch('/rating/getInstructorRatings')
+  //     .send({
+  //       instructorID: '12omj8v4kz4b0ot4',
+  //     });
+  //   expect(response.statusCode).toBe(200);
+  // });
 
   it('bad request, should respond with 500 status code', async () => {
     const response = await request(app)
-      .post('/rating/updateRating')
+      .patch('/rating/getInstructorRatings')
+      .send({ instructorID: new Error('Server Error') });
+    expect(response.statusCode).toBe(500);
+  });
+
+  // to modify
+  // it('rating updated, should respond with 200 status code', async () => {
+  //   const response = await request(app).patch('/rating/updateRating').send({
+  //     rating: 2,
+  //     userEmail: 'keilFinez@dlsu.edu.ph',
+  //     instructorEmail: 'paoloEspiritu@dlsu.edu.ph',
+  //   });
+  //   expect(response.statusCode).toBe(200);
+  // });
+
+  it('bad request, should respond with 500 status code', async () => {
+    const response = await request(app)
+      .patch('/rating/updateRating')
       .send({ error: new Error('Server Error') });
     expect(response.statusCode).toBe(500);
   });
