c4d72971c8413ec85301df96bd13ad4e0e6b2fec
feilkinez <75008462+feilkinez@users.noreply.github.com>
2022-02-04 22:12:20 +0800

    Revert "Will fix review"
    This reverts commit b2e8bdc008717fea17daedd795d51adaea534e88.

--- server/src/__test__/unit tests/review.test.js
+++ server/src/__test__/unit tests/review.test.js
@@ -1,21 +1,11 @@
 /* eslint-disable node/no-unpublished-import */
 import request from 'supertest';
 import app from '../../app.js';
-import mockDB from '../../config/mockDB.js';
+import mongoose from 'mongoose';
 
 describe('Test Index Route', () => {
-  /**
-   * Clear all test data after every test.
-   */
-  afterEach(async () => {
-    await mockDB.clearDatabase();
-  });
-
-  /**
-   * Remove and close the db and server.
-   */
   afterAll(async () => {
-    await mockDB.closeDatabase();
+    mongoose.disconnect();
   });
   it('function returns prof, should respond with 200 status code', async () => {
     await request(app)
@@ -29,7 +19,6 @@ describe('Test Index Route', () => {
         department: 'Software Technology',
         courses: ['STSWENG', 'CSSWENG'],
       });
-
     const response = await request(app).post('/review/reviews').send({
       instructorID: 'asidasio91213',
     });
