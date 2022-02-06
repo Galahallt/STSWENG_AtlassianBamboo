d8c594e7c8446ac51a3c9248fc5dec3673c9affa
Galahallt <paolo_edni_v_espiritu@dlsu.edu.ph>
2022-02-06 17:19:49 +0800

    fix logger

--- server/src/logger/index.js
+++ server/src/logger/index.js
@@ -1,10 +1,12 @@
 import buildDevLogger from './dev_logger.js';
 import buildProdLogger from './prod_logger.js';
+import dotenv from 'dotenv-safe';
+
+dotenv.config();
 
 let logger = null;
-const { NODE_ENV } = process.env;
 
-console.log(process.env.MONGO_URI);
+const { NODE_ENV } = process.env;
 
 if (NODE_ENV === 'development') {
   logger = buildDevLogger();
